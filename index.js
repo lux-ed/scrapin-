e377558df47243570dd88e7cc108bd3877f00555
lux-ed <lux-ed@users.noreply.github.com>
2018-11-29 23:25:14 +0100

    finished working file

--- index.js
+++ index.js
@@ -9,16 +9,26 @@ async function requestUrl(url) {
 
 
 function parseHtml(html) {
-    return html;
+    const selector = '.s-result-list > li h2';
+    const $ = cheerio.load(html);
+
+    const results = [];
+
+    const result = $(selector).each(function(i, elem) {
+        results.push($(elem).text());
+    });
+    return results;
 }
 
 
 function outputResults(parsedResults) {
-    console.log(parsedResults);
+    const resultsCount = parsedResults.length;
+    console.log('%d result(s) ->', resultsCount, parsedResults);
 }
 
 (async () => {
     const html = await requestUrl('https://www.amazon.es/s/ref=nb_sb_noss/257-5628384-8752308?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=phones');
+    
     const parsedResults = parseHtml(html);
 
     outputResults(parsedResults);
